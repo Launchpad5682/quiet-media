1cdfc4fc9f9ebf49709245fe89fef9c5a29006f6
launchpad5682 <launchpad5682@gmail.com>
2022-05-13 22:39:48 +0530

    feat: loading status while authenticating added

--- src/routes/Auth/AuthSlice.js
+++ src/routes/Auth/AuthSlice.js
@@ -3,12 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
   name: "authentication",
   initialState: {
+    loading: true,
     authenticated: false,
     user: { displayName: null, uid: null, photoURL: null, email: null },
   },
   reducers: {
     updateAuthenticated: (state, action) => {
-      state.authenticated = action.payload;
+      state.authenticated = action.payload.authenticated;
+      state.loading = action.payload.loading;
     },
     updateUserData: (state, action) => {
       state.user = action.payload;
