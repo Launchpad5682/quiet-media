008e5276edc7122a594cec7125757a6b6a6a4508
launchpad5682 <launchpad5682@gmail.com>
2022-05-13 22:39:14 +0530

    feat: Signup functionality added

---
+++ src/routes/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,58 @@
+import { SolidButton, InputField, OutlineButton } from "../../../common";
+import styles from "../Auth.module.scss";
+import { Logo } from "../Logo";
+import { useSignUp } from "./useSignUp";
+
+export const SignUp = () => {
+  const { formData, loading, changeHandler, submitHandler } = useSignUp();
+
+  return (
+    <div className={styles.main}>
+      <Logo />
+      <div className={styles.form__container}>
+        <div className="h6__typography typogrpahy--white bold--typography center__typography">
+          Signup
+        </div>
+        <form onSubmit={submitHandler} className={styles.form}>
+          <InputField
+            name="fullname"
+            type="text"
+            changeHandler={changeHandler}
+            label="Full Name"
+            value={formData.fullName}
+          />
+          <InputField
+            name="username"
+            type="text"
+            changeHandler={changeHandler}
+            label="Username"
+            value={formData.username}
+          />
+          <InputField
+            name="email"
+            type="email"
+            changeHandler={changeHandler}
+            label="E-mail"
+            value={formData.email}
+          />
+          <InputField
+            name="password"
+            type="password"
+            changeHandler={changeHandler}
+            label="Password"
+            value={formData.password}
+          />
+          <SolidButton
+            buttonText={loading ? "Signing up..." : "Signup"}
+            fullWidth={true}
+            disabled={loading}
+          />
+          <OutlineButton
+            buttonText="Already have an account"
+            fullWidth={true}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
---
+++ src/routes/Auth/SignUp/useSignUp.js
@@ -0,0 +1,75 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, firestoreDB } from "../../../firebase";
+
+export const useSignUp = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "fullname":
+        setFormData((prev) => ({ ...prev, fullName: value }));
+        break;
+      case "username":
+        setFormData((prev) => ({ ...prev, username: value }));
+        break;
+      case "email":
+        setFormData((prev) => ({ ...prev, email: value }));
+        break;
+      case "password":
+        setFormData((prev) => ({ ...prev, password: value }));
+        break;
+      default:
+        new Error("Not a valid input handler");
+    }
+  };
+
+  const createUser = async (fullName, username, email, password) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setLoading(false);
+      await updateProfile(auth.currentUser, {
+        displayName: fullName,
+        photoURL: "",
+      });
+      navigate("/home");
+      await addDoc(collection(firestoreDB, "users"), {
+        displayName: fullName,
+        photoURL: null,
+        backgroundImageURL: null,
+        bio: null,
+        portfolioURL: null,
+        posts: [],
+        followers: [],
+        following: [],
+        uid: auth.currentUser.uid,
+        bookmarkPosts: [],
+        username: username,
+        notifications: [],
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const { fullName, username, email, password } = formData;
+    setLoading(true);
+    await createUser(fullName, username, email, password);
+    setFormData({ fullName: "", username: "", email: "", password: "" });
+  };
+
+  return { formData, loading, changeHandler, submitHandler };
+};
