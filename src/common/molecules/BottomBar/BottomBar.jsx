de92fab7530e3c8fbb18352a12582a2533c8f391
launchpad5682 <launchpad5682@gmail.com>
2022-05-14 21:10:41 +0530

    feat: BottomBar added

--- src/common/molecules/BottomBar/BottomBar.jsx
+++ src/common/molecules/BottomBar/BottomBar.jsx
@@ -1,5 +1,25 @@
 import styles from "./BottomBar.module.scss";
 
+import { AiOutlineHome, AiOutlineRocket } from "react-icons/ai";
+import { BsBookmark } from "react-icons/bs";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { BottomBarNavLink } from "../../atoms/BottomBarNavLink/BottomBarNavLink";
+
 export const BottomBar = () => {
-  return <div></div>;
+  return (
+    <div className={styles.bottom__bar}>
+      <BottomBarNavLink path="/home">
+        <AiOutlineHome />
+      </BottomBarNavLink>
+      <BottomBarNavLink path="/explore">
+        <AiOutlineRocket />
+      </BottomBarNavLink>
+      <BottomBarNavLink path="/bookmarks">
+        <BsBookmark />
+      </BottomBarNavLink>
+      <BottomBarNavLink path="/notifications">
+        <IoNotificationsOutline />
+      </BottomBarNavLink>
+    </div>
+  );
 };
