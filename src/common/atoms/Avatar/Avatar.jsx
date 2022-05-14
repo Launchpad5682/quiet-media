106b0bc1a670b0c9056263fff470b8cb447c15ec
launchpad5682 <launchpad5682@gmail.com>
2022-05-14 21:08:12 +0530

    feat: Avatar component added

---
+++ src/common/atoms/Avatar/Avatar.jsx
@@ -0,0 +1,15 @@
+/**
+ * size: sm, md and lg
+ */
+
+export const Avatar = ({ size = "sm", imgURL }) => {
+  return (
+    <span className={`${size}-avatar round fit-image`}>
+      <img
+        src="https://images.unsplash.com/photo-1602870049368-9c8265ff3693?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+        className={`img-fit round ${size}-avatar`}
+        alt=""
+      />
+    </span>
+  );
+};
