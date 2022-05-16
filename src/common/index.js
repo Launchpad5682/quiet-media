b99ae193a096cf0eea752ce102b314551735fd6d
launchpad5682 <launchpad5682@gmail.com>
2022-05-16 19:45:58 +0530

    feat: index file updated

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,8 @@
 export { Header } from "./atoms/Header/Header";
+export { IconButton } from "./atoms/Button/IconButton/IconButton";
 export { InputField } from "./atoms/InputField/InputField";
 export { OutlineButton } from "./atoms/Button/OutlineButton/OutlineButton";
 export { SolidButton } from "./atoms/Button/SolidButton/SolidButton";
+export { Post } from "./molecules/Post/Post";
 
 export { Layout } from "./organisms/Layout/Layout";
