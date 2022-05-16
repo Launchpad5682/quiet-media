99ee9852810319dc0c4f2368627edafe27b4c7df
launchpad5682 <launchpad5682@pop-os.localdomain>
2022-05-16 12:49:03 +0530

    feat: Modal added to the App for post create and edit and user profile create and edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,38 @@ import { Login } from "./routes/Auth/Login/Login";
 import { SignUp } from "./routes/Auth/SignUp/SignUp";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-import { useDispatch } from "react-redux";
+import { auth, firestoreDB } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
 import { updateAuthenticated, updateUserData } from "./routes/Auth/AuthSlice";
 import { Home } from "./routes/Home/Home";
 import { Layout } from "./common";
 import { PrivateRoute } from "./helper/PrivateRoute";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { setUserInformation } from "./routes/UserProfile/UserSlice";
+import { Modal } from "./common/organisms/Modal/Modal";
 
 function App() {
   const dispatch = useDispatch();
+  const modal = useSelector((store) => store.modal);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { displayName, uid, photoURL, email } = user;
+
+        (async () => {
+          const docRef = query(
+            collection(firestoreDB, "users"),
+            where("uid", "==", uid)
+          );
+
+          const userInformation = await getDocs(docRef);
+          // it'll only have one doc
+          userInformation.forEach((doc) => {
+            console.log(doc.data());
+            dispatch(setUserInformation({ ...doc.data() }));
+          });
+        })();
         dispatch(updateAuthenticated({ authenticated: true, loading: false }));
         dispatch(updateUserData({ displayName, uid, photoURL, email }));
       } else {
@@ -39,6 +57,7 @@ function App() {
           element={
             <PrivateRoute>
               <Layout />
+              {modal.type !== null && <Modal />}
             </PrivateRoute>
           }
         >
