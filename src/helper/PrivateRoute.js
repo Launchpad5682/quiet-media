9bdaae76f0c4324a9cd264d4c753173412c6603d
launchpad5682 <launchpad5682@gmail.com>
2022-05-19 01:16:59 +0530

    feat: private route udpated and timeStamptoDisplay added

---
+++ src/helper/PrivateRoute.js
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { FullPageLoader } from "../common/organisms/FullPageLoader/FullPageLoader";
+
+export const PrivateRoute = ({ children }) => {
+  const authenticated = useSelector(
+    (state) => state.authentication.authenticated
+  );
+  const loading = useSelector((state) => state.authentication.loading);
+
+  if (loading) {
+    return <FullPageLoader />;
+  } else if (authenticated && !loading) {
+    return children;
+  }
+  return <Navigate to="/login" replace={true} />;
+};
---
+++ src/helper/timeStampToDisplay.js
@@ -0,0 +1,1 @@
+export const timeStampToDisplay = (timeStamp) => timeStamp.toDate().toString();
