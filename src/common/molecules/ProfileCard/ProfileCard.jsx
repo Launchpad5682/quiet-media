9c5586f84373b4d8656e85d5000d15b3647a0445
launchpad5682 <launchpad5682@gmail.com>
2022-05-19 01:24:10 +0530

    feat: UserProfile and Profile Card added

--- src/common/molecules/ProfileCard/ProfileCard.jsx
+++ src/common/molecules/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,36 @@
 import { Avatar } from "../../atoms/Avatar/Avatar";
 import { IoIosOptions } from "react-icons/io";
 import styles from "./ProfileCard.module.scss";
+import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { OptionItem } from "../../atoms/OptionItem/OptionItem";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useLogout } from "../../../hooks/useLogout";
+import { useOnClickOutside } from "../../../hooks/useOnClickOutside";
 
 export const ProfileCard = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const photoURL = useSelector((store) => store.userInformation.photoURL);
+  const dropdownRef = useRef();
+
+  const { logout } = useLogout();
+  const dropdownHandler = () => setDropdown((prev) => !prev);
+  useOnClickOutside(dropdownRef, dropdownHandler);
+  const logoutHandler = () => {
+    logout();
+    dropdownHandler();
+  };
+
   return (
-    <div className={styles.profile__card}>
-      <Avatar size="sm" />
+    <div className={styles.profile__card} onClick={dropdownHandler}>
+      {dropdown && (
+        <div className={styles.dropdown} ref={dropdownRef}>
+          <OptionItem clickHandler={logoutHandler}>
+            <AiOutlineLogout /> Logout
+          </OptionItem>
+        </div>
+      )}
+      <Avatar size="sm" imgURL={photoURL ? photoURL : ""} />
       <div className={styles.profile__name}>
         <span className="h6__typography bold--typography">Saurabh Suthar</span>
         <span className="subtitle1__typography">@saurabh22suthar</span>
