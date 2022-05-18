e56ffeaa8c6aa08a254462bf13898acc79bb1dc5
launchpad5682 <launchpad5682@gmail.com>
2022-05-19 01:25:35 +0530

    refactor: updated index file for components

--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,30 @@
+// atoms
+export { Avatar } from "./atoms/Avatar/Avatar";
+export { BottomBarNavLink } from "./atoms/BottomBarNavLink/BottomBarNavLink";
 export { Header } from "./atoms/Header/Header";
 export { IconButton } from "./atoms/Button/IconButton/IconButton";
 export { InputField } from "./atoms/InputField/InputField";
+export { Loader } from "./atoms/Loader/Loader";
+export { OptionItem } from "./atoms/OptionItem/OptionItem";
 export { OutlineButton } from "./atoms/Button/OutlineButton/OutlineButton";
+export { SideBarNavLink } from "./atoms/SideBarNavLink/SideBarNavLink";
 export { SolidButton } from "./atoms/Button/SolidButton/SolidButton";
-export { Post } from "./molecules/Post/Post";
+export { Tab } from "./atoms/Tab/Tab";
+export { TextBoxField } from "./atoms/TextBoxField/TextBoxField";
 
+// molecules
+export { BottomBar } from "./molecules/BottomBar/BottomBar";
+export { Comment } from "./molecules/Comment/Comment";
+export { CommentBox } from "./molecules/CommentBox/CommentBox";
+export { HomeTabs } from "./molecules/HomeTabs/HomeTabs";
+export { LeftSideBar } from "./molecules/LeftSideBar/LeftSideBar";
+export { Post } from "./molecules/Post/Post";
+export { ProfileCard } from "./molecules/ProfileCard/ProfileCard";
+export { RightSideBar } from "./molecules/RightSideBar/RightSideBar";
+export { TextBox } from "./molecules/TextBox/TextBox";
+export { UserProfile } from "./molecules/UserProfile/UserProfile";
+
+// organisms
+export { FullPageLoader } from "./organisms/FullPageLoader/FullPageLoader";
 export { Layout } from "./organisms/Layout/Layout";
+export { Modal } from "./organisms/Modal/Modal";
