9602514b65d7b02558c394c56bb1a6edd71bc078
launchpad5682 <launchpad5682@gmail.com>
2022-05-19 01:27:04 +0530

    refactor: updated store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { modalReducer } from "./common/organisms/Modal/ModalSlice";
 import { authReducer } from "./routes/Auth/AuthSlice";
-import { exploreReducer } from "./routes/Explore/ExploreSlice";
 import { homeReducer } from "./routes/Home/HomeSlice";
-import { userReducer } from "./routes/UserProfile/UserSlice";
+import { exploreReducer } from "./routes/Explore/ExploreSlice";
+import { userReducer } from "./routes/User/UserSlice";
 
 export const store = configureStore({
   reducer: {
