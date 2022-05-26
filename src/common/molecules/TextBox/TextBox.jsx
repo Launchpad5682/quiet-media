38d146ab879c1e878428adb39fed4c6fd44f7f14
launchpad5682 <launchpad5682@pop-os.localdomain>
2022-05-26 17:31:34 +0530

    feat: Disabled button when text field is empty

--- src/common/molecules/TextBox/TextBox.jsx
+++ src/common/molecules/TextBox/TextBox.jsx
@@ -26,6 +26,7 @@ export const TextBox = ({
   const [count, setCount] = useState(0);
   const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
+  const [empty, setEmpty] = useState(true);
 
   const [selectedFile, setSelectedFile] = useState(null);
   const userInformation = useSelector((store) => store.userInformation);
@@ -129,6 +130,14 @@ export const TextBox = ({
     }
   };
 
+  useEffect(() => {
+    if (value.length === 0) {
+      setEmpty(true);
+    } else {
+      setEmpty(false);
+    }
+  }, [value]);
+
   return (
     <div className={styles.text__box}>
       <div className={styles.avatar__container}>
@@ -172,7 +181,7 @@ export const TextBox = ({
             />
           </div>
           <SolidButton
-            disabled={loading}
+            disabled={loading || empty}
             buttonText={loading ? "Posting..." : edit ? "Edit Post" : "Post"}
             clickHandler={() => submitHandler(value)}
           />
